16930e9513f1f76b60a9a5f13e0a7cd8430f0080
Lahiru Amarasooriya <lahirudx@gmail.com>
2023-07-09 10:55:28 +0530

    Updated to new if let syntax in swift

--- Appetizers/Utilities/Extentions/String+Ext.swift
+++ Appetizers/Utilities/Extentions/String+Ext.swift
@@ -6,13 +6,45 @@
 //
 
 import Foundation
+import RegexBuilder
 
 extension String {
     
     var isValidEmail: Bool {
-        let emailFormat = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"
-        let emailPredicate      = NSPredicate(format: "SELF MATCHES %@", emailFormat)
-        print(self)
-        return emailPredicate.evaluate(with: self)
+//        let emailFormat = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"
+//        let emailPredicate      = NSPredicate(format: "SELF MATCHES %@", emailFormat)
+//        print(self)
+//        return emailPredicate.evaluate(with: self)
+        
+        let emailFormat = Regex {
+            /^/
+            OneOrMore {
+                CharacterClass(
+                    .anyOf("._%+-"),
+                    ("a"..."z"),
+                    ("A"..."Z"),
+                    ("0"..."9")
+                )
+            }
+            "@"
+            OneOrMore {
+                CharacterClass(
+                    .anyOf(".-"),
+                    ("a"..."z"),
+                    ("A"..."Z"),
+                    ("0"..."9")
+                )
+            }
+            "."
+            Repeat(2...64) {
+                CharacterClass(
+                    ("a"..."z"),
+                    ("A"..."Z")
+                )
+            }
+            /$/
+        }
+        
+        return self.wholeMatch(of: emailFormat) != nil
     }
 }
